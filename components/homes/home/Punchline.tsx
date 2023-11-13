11314ec3642b4046543a4b2fd8a2fb981af6bb35
M4x <maxmurray1999@gmail.com>
2023-11-13 08:58:06 +0700

    trying to fix alginment

--- components/homes/home/Punchline.tsx
+++ components/homes/home/Punchline.tsx
@@ -13,39 +13,43 @@ const Punchline: React.FC<title_content_type> = ({ line1, line2 }) =>  {
 
     if (line2 === "") {
         return (
-            <section>
-                <section className="core-features-area white-bg pt-135 pb-20">
-                    <div className="container">
-                        <div className="row align-items-center mb-70">
-                            <div>
-                                <div className="section-title text-center mb-20">
-                                    <h2>{line1}</h2>
-                                </div>
+<>
+            <section className="pt-100 pb-40">
+                <div className="container">
+                    <div className="row ">
+                        <div className="col-xl-6 offset-xl-3">
+                            <div className="section-title text-center mb-75">
+                                <h2>{line1}</h2>
+                                {/* <p>{sm_des}</p> */}
                             </div>
                         </div>
                     </div>
-                </section>
+                    {/* <DomainSearchBox />  */}
+                </div>
             </section>
+        </>
         );
     } else
     return (
-        <section>
-            <section className="core-features-area white-bg pt-50 pb-60">
+        <>
+            <section className="pt-100 pb-40">
                 <div className="container">
-                    <div className="row align-items-center mb-40">
-                        <div>
-                            <div className="section-title text-center mb-40">
+                    <div className="row ">
+                        <div className="col-xl-6 offset-xl-3">
+                            <div className="section-title text-center mb-75">
                                 <h2>{line1}</h2>
+                                {/* <p>{sm_des}</p> */}
                             </div>
-                            <div className="section-title text-center mb-40">
-                            <h2>{line2}</h2>
+                            <div className="section-title text-center mb-75">
+                                <h2>{line2}</h2>
+                                {/* <p>{sm_des}</p> */}
                             </div>
                         </div>
                     </div>
+                    {/* <DomainSearchBox />  */}
                 </div>
             </section>
-        </section>
-    );
+        </>
 };
 
 export default Punchline;
