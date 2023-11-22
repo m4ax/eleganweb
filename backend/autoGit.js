02581288f6a0faa0d98bb8934c819fa32df58222
M4x <maxmurray1999@gmail.com>
2023-11-22 14:40:53 +0700

    Automated blog update

---
+++ backend/autoGit.js
@@ -0,0 +1,25 @@
+import { promisify } from 'util';
+import { exec as execCallback } from 'child_process';
+
+const exec = promisify(execCallback);
+
+async function gitCommitAndPush() {
+  try {
+    // Add changes to Git
+    await exec('git add -A');
+    console.log('Changes added to git.');
+
+    // Commit changes
+    await exec('git commit -m "Automated blog update"');
+    console.log('Changes committed.');
+
+    // Push changes to the repository
+    await exec('git push');
+    console.log('Changes pushed to GitHub.');
+  } catch (error) {
+    console.error('Error during Git operations:', error);
+  }
+}
+
+// Call this function after blog content generation
+gitCommitAndPush();
