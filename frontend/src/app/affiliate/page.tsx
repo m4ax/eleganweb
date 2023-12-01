ca7d28ab476d6ed7270bd6df0808c8a5b28f96ed
M4x <maxmurray1999@gmail.com>
2023-12-01 11:46:24 +0700

    updated title and desc for affiliate, testing..

--- frontend/src/app/affiliate/page.tsx
+++ frontend/src/app/affiliate/page.tsx
@@ -1,8 +1,13 @@
 import Affiliate from "../../../components/affiliate";
+import CustomHead from "../../../components/custom-head";
 
 const index = () => {
   return (
     <>
+      <CustomHead
+        title="Affiliate Scheme | Data Migration Services"
+        description="Learn how you can earn commission through affiliate sales. We offer 20% of ongoing revenue for the lifetime of the customer."
+        />
       <Affiliate />
     </>
   );
